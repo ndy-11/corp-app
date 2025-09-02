4b272ee06379e7ffdd573f9a2d8425841ab11725
Ndy-11 <sandyirawan53@gmail.com>
2025-09-02 17:15:34 +0700

    test update

--- app/Models/MeetingMinute.php
+++ app/Models/MeetingMinute.php
@@ -8,4 +8,33 @@ use Illuminate\Database\Eloquent\Model;
 class MeetingMinute extends Model
 {
     use HasFactory;
+
+    public $fillable = [
+        'meeting_id',
+        'content',
+        'created_by',
+    ];
+
+    public function meeting()
+    {
+        return $this->belongsTo(Meeting::class);
+    }
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+    public function attachments()
+    {
+        return $this->morphMany(Attachment::class, 'attachable');
+    }
+
+    public function getContentAttribute($value)
+    {
+        return nl2br(e($value));
+    }
+
+    public function setContentAttribute($value)
+    {
+        $this->attributes['content'] = strip_tags($value, '<p><a><br><b><i><strong><em><ul><ol><li>');
+    }
 }
